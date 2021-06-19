d30e66473dd2a5537ceea1b48680a4ab2ff887cb
SEHRISH GULZAR <sehrigul11@gmail.com>
2021-06-19 12:26:25 -0700

    upload

--- lib/ecom.dart
+++ lib/ecom.dart
@@ -18,114 +18,116 @@ class _EcomState extends State<Ecom> {
         ),
         backgroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            margin: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.grey[300],
-              image: DecorationImage(
-                alignment: Alignment.topLeft,
-                image: AssetImage('assets/a.jpg'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.grey[300],
+                image: DecorationImage(
+                  alignment: Alignment.topLeft,
+                  image: AssetImage('assets/a.jpg'),
+                ),
               ),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Apple Laptop\n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+              padding: EdgeInsets.only(right: 60),
             ),
-            child: Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "Apple Laptop\n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-            padding: EdgeInsets.only(right: 60),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            margin: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.grey[300],
-              image: DecorationImage(
-                alignment: Alignment.topLeft,
-                image: AssetImage('assets/b.jpg'),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.grey[300],
+                image: DecorationImage(
+                  alignment: Alignment.topLeft,
+                  image: AssetImage('assets/b.jpg'),
+                ),
               ),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "MacBook Pro \n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+              padding: EdgeInsets.only(right: 60),
             ),
-            child: Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "MacBook Pro \n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-            padding: EdgeInsets.only(right: 60),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            margin: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.grey[300],
-              image: DecorationImage(
-                alignment: Alignment.topLeft,
-                image: AssetImage('assets/c.jpg'),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.grey[300],
+                image: DecorationImage(
+                  alignment: Alignment.topLeft,
+                  image: AssetImage('assets/c.jpg'),
+                ),
               ),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Iphone Laptop  \n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+              padding: EdgeInsets.only(right: 60),
             ),
-            child: Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "Iphone Laptop  \n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-            padding: EdgeInsets.only(right: 60),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            margin: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.grey[300],
-              image: DecorationImage(
-                alignment: Alignment.topLeft,
-                image: AssetImage('assets/d.jpg'),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.grey[300],
+                image: DecorationImage(
+                  alignment: Alignment.topLeft,
+                  image: AssetImage('assets/d.jpg'),
+                ),
               ),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Iphone 12 \n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+              padding: EdgeInsets.only(right: 60),
             ),
-            child: Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "Iphone 12 \n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-            padding: EdgeInsets.only(right: 60),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            margin: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.grey[300],
-              image: DecorationImage(
-                alignment: Alignment.topLeft,
-                image: AssetImage('assets/e.jpg'),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.grey[300],
+                image: DecorationImage(
+                  alignment: Alignment.topLeft,
+                  image: AssetImage('assets/e.jpg'),
+                ),
               ),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Samsung Note 20\n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+              padding: EdgeInsets.only(right: 60),
             ),
-            child: Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "Samsung Note 20\n 5.0(23 Reviews) \n 20 Pieces    \$90 \n Quality:1 ",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-            padding: EdgeInsets.only(right: 60),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Back")),
-        ],
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Back")),
+          ],
+        ),
       ),
     );
   }
